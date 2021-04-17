06cf23bf41c84fd5df248035e2d464345b3de963
ypolcum <65310765+ypolcum@users.noreply.github.com>
2021-04-16 22:13:09 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Exam2EC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.codeLbl = new System.Windows.Forms.Label();
+            this.codeTxt = new System.Windows.Forms.TextBox();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.outputNameLbl = new System.Windows.Forms.Label();
+            this.findNameBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // codeLbl
+            // 
+            this.codeLbl.AutoSize = true;
+            this.codeLbl.Location = new System.Drawing.Point(14, 16);
+            this.codeLbl.Name = "codeLbl";
+            this.codeLbl.Size = new System.Drawing.Size(102, 13);
+            this.codeLbl.TabIndex = 0;
+            this.codeLbl.Text = "Enter Country Code:";
+            // 
+            // codeTxt
+            // 
+            this.codeTxt.Location = new System.Drawing.Point(122, 13);
+            this.codeTxt.Name = "codeTxt";
+            this.codeTxt.Size = new System.Drawing.Size(100, 20);
+            this.codeTxt.TabIndex = 1;
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(39, 46);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(77, 13);
+            this.nameLbl.TabIndex = 2;
+            this.nameLbl.Text = "Country Name:";
+            // 
+            // outputNameLbl
+            // 
+            this.outputNameLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputNameLbl.Location = new System.Drawing.Point(122, 45);
+            this.outputNameLbl.Name = "outputNameLbl";
+            this.outputNameLbl.Size = new System.Drawing.Size(100, 20);
+            this.outputNameLbl.TabIndex = 3;
+            // 
+            // findNameBtn
+            // 
+            this.findNameBtn.Location = new System.Drawing.Point(105, 76);
+            this.findNameBtn.Name = "findNameBtn";
+            this.findNameBtn.Size = new System.Drawing.Size(75, 23);
+            this.findNameBtn.TabIndex = 4;
+            this.findNameBtn.Text = "Find Name";
+            this.findNameBtn.UseVisualStyleBackColor = true;
+            this.findNameBtn.Click += new System.EventHandler(this.findNameBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.findNameBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.findNameBtn);
+            this.Controls.Add(this.outputNameLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.codeTxt);
+            this.Controls.Add(this.codeLbl);
+            this.Name = "Form1";
+            this.Text = "Exam 2 EC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label codeLbl;
+        private System.Windows.Forms.TextBox codeTxt;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label outputNameLbl;
+        private System.Windows.Forms.Button findNameBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿// Exam 2, Part 3
+// CIS 199-XX
+// Spring 2019
+// Prompt the user to enter country code and then look up
+// matching country name and display. If no match is found,
+// display "No Match!" instead.
+
+// By: Andrew L. Wright
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam2EC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetCountryName(int countryCode)
+        {
+            int[] codes = { 1, 44, 354, 91, 353, 81, 53, 27, 52 }; // The country codes
+            string[] names = { "United States", "United Kingdom", "Iceland", "India", "Ireland",
+                                 "Japan", "Cuba", "South Africa", "Mexico" }; // The associated countries
+
+            string countryName = "No match!"; // Matching country name, assumes not found
+            int index = 0;                    // Current position being searched, start at first subscript
+            bool found = false;               // Have we found a match yet?
+
+            while (index < codes.Length && !found)
+            {
+                if (countryCode == codes[index])
+                    found = true;
+                else
+                    ++index;
+            }
+
+            if (found)
+                countryName = names[index];
+
+            return countryName;
+        }
+
+        private void findNameBtn_Click(object sender, EventArgs e)
+        {
+            int countryCode;
+            string countryName;
+
+            if (int.TryParse(codeTxt.Text, out countryCode))
+            {
+                countryName = GetCountryName(countryCode);
+                outputNameLbl.Text = countryName;
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid country code!");
+            }
+
+        }
+    }
+}
